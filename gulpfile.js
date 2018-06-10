088d189082fcef01975936355078795b79983b1e
MaxGrigorev <MaxGrigorev@github.com>
2018-06-11 02:25:08 +0500

    gulp-less

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,12 @@ const useref = require('gulp-useref')
 const cleanCSS = require('gulp-clean-css')
 const gulpif = require('gulp-if')
 const uglify = require('gulp-uglify')
+var less = require('gulp-less');
 
-gulp.task('build', () => {
-	return gulp.src('./source/**/*')
+gulp.task('build', ['less'], () => {
+	gulp.src(["!./source/less/**/*", './source/**/*'])
 		//.pipe(useref())
-		.pipe(gulpif('*.min.css', cleanCSS()))
+		.pipe(gulpif('*.css', cleanCSS()))
 		.pipe(gulp.dest('./build'))
 })
 
@@ -27,3 +28,9 @@ gulp.task('browser-sync', () => {
 gulp.task('reload', () => {
 	return browserSync.reload()
 })
+
+gulp.task('less', function () {
+	return gulp.src('./source/less/styles.less')
+		.pipe(less())
+		.pipe(gulp.dest('./source/css'));
+});
